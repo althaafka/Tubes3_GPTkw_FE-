4b648f895c3d668561ced64f8b2ff3b636e2ffe2
althaafka <althaafka004@gmail.com>
2023-04-23 19:14:19 +0700

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import Sidebar from '../components/Sidebar'
+import Chat from '../components/Chat'
+
+
+export default function Home() {
+  return (
+    <div className="App">
+      <Chat />
+      <Sidebar />
+    </div>
+  )
+}
